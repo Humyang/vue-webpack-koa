3b29782d17065d665faedf4b1ffbd7efd99aef71
Your Name <378029633@qq.com>
2018-05-11 16:58:04 +0800

    更新

---
+++ template/view/index/index.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+    <p>欢迎光临</p>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "index"
+};
+</script>
+
